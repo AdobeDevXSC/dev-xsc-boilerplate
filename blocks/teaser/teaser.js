1bea40423dacf77c4a0e15252cdd9dea1375c931
KP Parrish <krparrish@adobe.com>
2024-12-06 17:36:14 -0700

    added teaser blockstyles

---
+++ blocks/teaser/teaser.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+  const blockImages = block.querySelectorAll('picture');
+
+  // media query match that indicates mobile/tablet width
+  const isDesktop = window.matchMedia('(min-width: 767px)');
+
+  function toggleImage() {
+    const mobileImage = blockImages[1];
+    const desktopImage = blockImages[0];
+
+    if (isDesktop.matches) {
+      mobileImage.closest('div').className = 'hidden';
+      desktopImage.closest('div').className = '';
+    } else {
+      desktopImage.closest('div').className = 'hidden';
+      mobileImage.closest('div').className = '';
+    }
+  }
+
+  isDesktop.addEventListener('change', () => toggleImage());
+}
