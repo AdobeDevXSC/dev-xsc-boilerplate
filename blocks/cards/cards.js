0bf310f1683f77823c8a1f46f37b437ddb68c4a1
KP Parrish <krparrish@adobe.com>
2024-12-06 20:55:57 -0700

    updated cards to use stories

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,4 +1,5 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import fetchHtml from '../story/story-cards-util.js';
 
 export default async function decorate(block) {
   const isJSON = block.classList.contains('is-json');
@@ -16,66 +17,6 @@ export default async function decorate(block) {
   }
 
   const ul = document.createElement('ul');
-
-  // code for fetching html from story pages
-  async function fetchHtml(url) {
-    const obj = {
-      method: 'get',
-      headers: new Headers({
-        'Content-Type': 'text/html',
-      }),
-      credentials: 'include',
-    };
-
-    let story = await fetch(url, obj);
-    story = await story.text();
-
-    const parser = new DOMParser();
-    const parsedStory = parser.parseFromString(story, 'text/html');
-
-    // Select specific elements from the parsed HTML
-    const heroPicture = parsedStory.querySelector('.hero.story picture');
-    const storyTitle = parsedStory.querySelector('h1');
-
-    // video link
-    const heroLinks = parsedStory.querySelectorAll('.hero.story a');
-    const videoLink = Array.from(heroLinks).filter((link) => link.href.endsWith('.mp4'))[0];
-
-    // industry tag(s)
-    const strongEls = parsedStory.querySelectorAll('.columns.quote p strong');
-    const industryMatch = Array.from(strongEls).find((el) => el.textContent.includes('Industry'));
-
-    const industryTags = [];
-    if (industryMatch && industryMatch !== undefined) {
-      const paraEl = industryMatch.closest('p');
-      const nestedLinks = paraEl.querySelectorAll('a');
-      nestedLinks.forEach((el) => {
-        industryTags.push(el.innerText);
-      });
-    }
-
-    const tagWrapper = document.createElement('div');
-    tagWrapper.classList.add('tag-wrapper');
-    industryTags.forEach((tag) => {
-      const span = document.createElement('span');
-      span.textContent = tag;
-      tagWrapper.append(span);
-    });
-
-    const storyLi = document.createElement('li');
-    storyLi.innerHTML = `
-      ${heroPicture.outerHTML}
-      <div class="story-card content-wrapper">
-        ${tagWrapper.outerHTML}
-        <h4>${storyTitle.innerHTML}</h4>
-        <div class="buttons-container">
-          <a class="video-btn button" href=${videoLink.href} target="_blank">Watch Video</a>
-          <a class="read-story button" href=${url}>Read Story</a>
-        </div>
-      </div>
-    `;
-    ul.append(storyLi);
-  }
 
   if (isStoryLinks) {
     const blockLinks = block.querySelectorAll('a');
